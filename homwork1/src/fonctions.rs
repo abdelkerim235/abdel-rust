a87ad452e9fd0481a8f72edc09c14d8e66154dc9
abdelkerim235 <nedjimetab235@gmail.com>
2023-07-06 10:38:26 +0300

    adding file

---
+++ homwork1/src/fonctions.rs
@@ -0,0 +1,13 @@
+// Create a new instance of Parserpub fn new(expr: &'a str) ->
+Result<Self, ParseError> {
+let mut lexer = Tokenizer::new(expr);
+let cur_token = match lexer.next() {
+Some(token) => token,
+None => return Err(ParseError::InvalidOperator
+("Invalid character".into())),
+};
+Ok(Parser {
+tokenizer: lexer,
+current_token: cur_token,
+})
+}
---
+++ homwork1/src/primitive_types.rs
@@ -0,0 +1,30 @@
+// Construct AST node for numbers, taking into account
+// negative prefixes while handling parenthesis
+fn parse_number(&mut self) -> Result<Node, ParseError> {
+let token = self.current_token.clone();
+match token {
+Token::Subtract => {
+self.get_next_token()?;
+let expr = self.generate_ast(OperPrec::Negative)?;
+Ok(Node::Negative(Box::new(expr)))
+}
+Token::Num(i) => {
+self.get_next_token()?;
+Ok(Node::Number(i))
+}
+Token::LeftParen => {
+self.get_next_token()?;
+let expr = self.generate_ast
+(OperPrec::DefaultZero)?;
+self.check_paren(Token::RightParen)?;
+if self.current_token == Token::LeftParen {
+let right = self.generate_ast
+(OperPrec::MulDiv)?return Ok(Node::Multiply(Box::new(expr),
+Box::new(right)));
+}
+Ok(expr)
+}
+_ => Err(ParseError::UnableToParse("Unable to
+parse".to_string())),
+}
+};
