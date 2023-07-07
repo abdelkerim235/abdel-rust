6951033c1ae07b1d89913c08718a05771d3e508c
abdelkerim235 <nedjimetab235@gmail.com>
2023-07-07 13:19:04 +0300

    ci adding

--- homwork2/src/task1.rs
+++ homwork2/src/task1.rs
@@ -11,13 +11,19 @@ const QUOTE: &str = "Every face, every shop, bedroom window, public-house, and
 dark square is a picture feverishly turned--in search of what?
 It is the same with books. What do we seek through millions of pages?";
 
-
-#[test]
-fn correct_line() {
-    let answer = find_term(SEARCH_TERM, QUOTE);
-
-    assert_eq!("2: dark square is a picture feverishly turned--in search of what?", answer)
+fn main() {
+    let result = find_term(SEARCH_TERM, QUOTE);
+    println!("{}", result);
 }
+
+fn find_term(search_term: &str, quote: &str) -> String {
+    let lines: Vec<&str> = quote.lines().collect();
+    for (index, line) in lines.iter().enumerate() {
+        if line.contains(search_term) {
+            return format!("{}: {}", index + 1, line.trim());
+        }
+    }
+    String::new()
 }
 
 
--- homwork2/src/task2.rs
+++ homwork2/src/task2.rs
@@ -10,26 +10,34 @@
  */
 
 
+fn main() {}
 
-[test]
-fn bottom_right() {
-    let rect = Rect::new((1., 2.), 5.);
-
-    assert_eq!((6., 7.), rect.bottom_right())
+struct Rect {
+    top_left: (f32, f32),
+    width: f32,
 }
 
-#[test]
-fn area() {
-    let rect = Rect::new((1., 2.), 5.);
+impl Rect {
+    fn new(top_left: (f32, f32), width: f32) -> Self {
+        Rect {
+            top_left,
+            width,
+        }
+    }
 
-    assert_eq!(25., rect.area())
-}
+    fn bottom_right(&self) -> (f32, f32) {
+        let bottom_right_x = self.top_left.0 + self.width;
+        let bottom_right_y = self.top_left.1 + self.width;
+        (bottom_right_x, bottom_right_y)
+    }
 
-#[test]
-fn perimeter() {
-    let rect = Rect::new((1., 2.), 5.);
+    fn area(&self) -> f32 {
+        self.width * self.width
+    }
 
-    assert_eq!(20., rect.perimeter())
+    fn perimeter(&self) -> f32 {
+        4.0 * self.width
+    }
 }
 
 // ----> TESTS
