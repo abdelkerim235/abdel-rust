f27983ed37e51183d844253c3d2389c073a0349c
abdelkerim235 <nedjimetab235@gmail.com>
2023-07-11 01:09:42 +0300

    adding file

--- homwork1/src/if.rs
+++
@@ -1,9 +0,0 @@
-// Take an arithmetic expression as input and return an AST
-
-# pub fn parse(&mut self) -> Result<Node, ParseError> {
-    let ast = self.generate_ast(OperPrec::DefaultZero);
-    match ast {
-        Ok(ast) => Ok(ast),
-        Err(e) => Err(e),
-    }
-}
